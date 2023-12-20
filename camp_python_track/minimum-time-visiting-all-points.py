1c07e9f2072d46e2a569532fb45eab62559f72e1
CamelsCase <154346115+CamelsCase@users.noreply.github.com>
2023-12-20 17:24:39 -0300

    Added solution for Minimum Time Visiting All Points

--- camp_python_track/minimum-time-visiting-all-points.py
+++ camp_python_track/minimum-time-visiting-all-points.py
@@ -3,9 +3,7 @@ class Solution:
         x,y = points[0]
         travel_sec = 0
         for i in range(len(points)):
-            xd = abs(x-points[i][0])
-            yd = abs(y-points[i][1])
+            travel_sec+=max(abs(x-points[i][0]),abs(y-points[i][1]))
             x,y = points[i]
-            travel_sec+=max(xd,yd)
         return travel_sec
         
