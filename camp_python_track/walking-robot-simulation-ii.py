f452cfd5492103d52b1080a15d1f4ac359e63ba5
CamelsCase <154346115+CamelsCase@users.noreply.github.com>
2023-12-25 14:48:31 -0300

    Added solution for Walking Robot Simulation II

--- camp_python_track/walking-robot-simulation-ii.py
+++ camp_python_track/walking-robot-simulation-ii.py
@@ -1,38 +1,18 @@
 class Robot:
-
     def __init__(self, width: int, height: int):
         self.ps = {0:"East",1:"North",2:"West",3:"South"}
         self.face = 0
         self.pos = [0,0]
         self.width = width
         self.height = height
-        
+
     def step(self, num: int) -> None:
-        """
-        here whene rc==0 and num>0 this means that the robot will come around and be in the previous positions it was.
-        but the catch is if that it may be facing other direction depending on what rout it took if the robot was on the corners the facing will change
-        not finished it also depend on ehich direction the robot is facing right now to
-        east[0,0] the botton left corner ===> south[0,0]
-        norht[x,y] the top right corner  ===> 
-        """
         rc = num%(2*self.width+2*self.height-4)
         if rc==0 and num>0:
-            # print("face before ",self.face,self.ps[self.face])
-            if self.pos==[0,0]:
-                if self.face ==0:
-                    self.face = 3
-            elif  self.pos==[0,self.height]:
-                if self.face ==3:
-                    self.face = 2
-            elif self.pos==[self.width,0]:
-                if self.face ==1:
-                    self.face = 0
-            elif self.pos==[self.width,self.height]:
-                if self.face ==2:
-                    self.face = 1
-            # print("face after ",self.face,self.ps[self.face])
+            if (self.pos==[0,0] and self.face ==0) or (self.pos==[0,self.height] and  self.face ==3) or (self.pos==[self.width,0] and  self.face ==1) or (self.pos==[self.width,self.height] and  self.face ==2):
+                    self.face = (self.face+3)%4
+
         for i in range(rc):
-            # print(i,self.pos,self.ps[self.face])
             found = False
             while not found:
                 if self.face==0:
@@ -59,10 +39,9 @@ class Robot:
                         found=True
                     else:
                         self.face = (self.face+1)%4
-                # print(self.pos)
+
     def getPos(self) -> list[int]:
         return self.pos
-        
 
     def getDir(self) -> str:
         return self.ps[self.face]
