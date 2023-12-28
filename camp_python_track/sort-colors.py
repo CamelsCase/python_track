e9a70b18bde17f2d953b7349131c1fc13297b08d
CamelsCase <154346115+CamelsCase@users.noreply.github.com>
2023-12-28 12:56:29 -0300

    Added solution for Sort Colors

--- camp_python_track/sort-colors.py
+++ camp_python_track/sort-colors.py
@@ -1,3 +1,8 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
+        freq = Counter(nums)
+        ind = 0
+        for i in {0,1,2}:
+            for fr in range(freq[i]):
+                nums[ind] = i
+                ind+=1
