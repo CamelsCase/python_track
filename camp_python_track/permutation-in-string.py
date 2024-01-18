aef37de35762214951d5b74f4209a523780b7515
CamelsCase <154346115+CamelsCase@users.noreply.github.com>
2024-01-18 17:06:42 -0300

    Added solution for Permutation in String

---
+++ camp_python_track/permutation-in-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq1 = Counter(s1)
+        leng = sum(freq1.values())
+        for i in range(len(s2)):
+            if Counter(s2[i:i+leng])==freq1:
+                return True
+        return False
