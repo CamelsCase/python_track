9deb8bf1406416ef1bb907e7c32ffa9e6f3ab53a
CamelsCase <154346115+CamelsCase@users.noreply.github.com>
2024-02-20 13:32:38 -0300

    Add solution for Merge Two Sorted Lists

--- camp_python_track/leetcode/merge-two-sorted-lists.py
+++ camp_python_track/leetcode/merge-two-sorted-lists.py
@@ -4,23 +4,28 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def do_the_magic(self,hd,merged,head1,head2):
+        if merged ==None:
+            merged = ListNode()
+        if hd==None:
+            hd = merged
+        if head1==None:
+            merged.next = head2
+            return hd.next
+        elif head2==None:
+            merged.next = head1
+            return hd.next
+        elif head1.val<head2.val:
+            merged.next = head1
+            head1 = head1.next
+        elif head1.val>=head2.val:
+            merged.next = head2
+            head2 = head2.next
+        merged = merged.next
+        next_call = self.do_the_magic(hd,merged,head1,head2)
+        return hd.next
+        
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        nw = ListNode(-102)
-        cur1 = list1
-        cur2 = list2
-        head = nw
-        while cur1 and cur2:
-            if cur1 and cur2:
-                if cur1.val<=cur2.val:
-                    nw.next = cur1
-                    nw = nw.next
-                    cur1 = cur1.next
-                else:
-                    nw.next = cur2
-                    nw = nw.next
-                    cur2 = cur2.next 
-        if cur1:
-            nw.next = cur1
-        elif cur2:
-            nw.next = cur2
-        return head.next
+        ls = self.do_the_magic(None,None,list1,list2)
+        return ls
+    
