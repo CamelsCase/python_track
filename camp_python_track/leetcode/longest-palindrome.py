eef9493c1bc4b10458a6c82a427f08db4ea3f713
CamelsCase <154346115+CamelsCase@users.noreply.github.com>
2024-02-20 13:31:09 -0300

    Add solution for Longest Palindrome

--- camp_python_track/leetcode/longest-palindrome.py
+++ camp_python_track/leetcode/longest-palindrome.py
@@ -1,11 +1,9 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         freq = Counter(s).most_common()
-        print(freq)
         take_odds = True
         leng = 0
         for i in freq:
-            print(i[0],i[1],leng)
             if  i[1]%2==1:
                 if take_odds:
                     leng+=i[1]
