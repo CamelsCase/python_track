7942e3152c097263fb3e98ccdde3e7920d286c42
CamelsCase <154346115+CamelsCase@users.noreply.github.com>
2024-03-03 05:44:54 -0300

    Add solution for N-Queens II

---
+++ camp_python_track/leetcode/n-queens-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        path = []
+        ans = []
+        attackable_row = set()
+        up_diagonal = set()
+        down_diagonal = set()
+        def find(row,col,rem):
+            nonlocal path
+            nonlocal attackable_row        
+            nonlocal up_diagonal
+            nonlocal down_diagonal
+            if len(path)==n and rem==0:
+                found = path[:]
+                if not found in ans:
+                    ans.append(found)
+                return 
+            for r in range(n):
+                if not r in attackable_row:
+                    if not r+col in up_diagonal and  not r-col in down_diagonal:
+                        x = ["."]*n
+                        x[r] ="Q"
+                        path.append("".join(x))
+                        attackable_row.add(r)
+                        up_diagonal.add(r+col)
+                        down_diagonal.add(r-col)
+                        find(r,col+1,rem-1)
+                        path.pop()
+                        attackable_row.remove(r)
+                        up_diagonal.remove(r+col)
+                        down_diagonal.remove(r-col)
+        find(0,0,n)
+        return len(ans)
+
+
+
