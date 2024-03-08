b6fd5ff2ee4dda20cecb9e6b547e698712f1d1bf
CamelsCase <154346115+CamelsCase@users.noreply.github.com>
2024-03-08 10:20:46 -0300

    Add solution for Decode String

--- camp_python_track/leetcode/decode-string.py
+++ camp_python_track/leetcode/decode-string.py
@@ -1,30 +1,21 @@
 class Solution:
-    def rec(self,n,s):
-        if n==0:
-            return ""
-        n-=1
-        return s+self.rec(n,s)            
     def decodeString(self, s: str) -> str:
+        
         stack = []
         for i in s:
-            if i!="]":
-                stack.append(i)
-            else:
-                val = []
-                while stack and not stack[-1].isdigit():
-                    ch = stack.pop()
-                    if ch!="[":
-                        val.append(ch)
-                times = []
+            if i=="]":
+                q = deque()
+                while stack and stack[-1]!="[":
+                    q.appendleft(stack.pop())
+                stack.pop()
+                x = deque()
                 while stack and stack[-1].isdigit():
-                    times.append(stack.pop())
-                times = int("".join(times[::-1]))
-                n = "".join(val[::-1])
-                stack.append(self.rec(times,n))
-        out = []
-        for i in stack:
-            out.append(i)
-        return "".join(out)
-        
-        
-            
+                    x.appendleft(stack.pop())
+                word = "".join(q)
+                mult = int("".join(x))
+                stack.append(word*mult)
+            else:
+                stack.append(i)
+        return "".join(stack)
+
+                
